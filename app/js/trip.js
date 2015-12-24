fb889d410206b9146d49391fc02acd971d14e884
Austin Heiman <atheimanksu@gmail.com>
2015-12-24 00:24:50 -0600

    map working, abandoned bower for cdns

--- app/js/trip.js
+++ app/js/trip.js
@@ -1,51 +1,116 @@
 angular.module('trip', [])
 
-.factory('googleMapsService', ['$http', 'GMAPS_KEY',
-  function($http, GMAPS_KEY){
+.factory('googleMapsService', ['$http', '$rootScope', '$timeout', 'GMAPS_KEY',
+  function($http, $rootScope, $timeout, GMAPS_KEY){
     // Used to query Google Maps API
-    var data = {};
+    var data = {
+      origin: {},
+      destination: {},
+      distance: {},
+      duration: {},
+    };
+
+    // Some google maps vars
+    var distMatService = new google.maps.DistanceMatrixService;
+    var geocoder = new google.maps.Geocoder;
+    var bounds = new google.maps.LatLngBounds;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 38.9822, lng: -94.6708},
+      zoom: 4,
+    });
+    var destinationIcon = 'https://chart.googleapis.com/chart?' +
+        'chst=d_map_pin_letter&chld=D|FF0000|000000';
+    var originIcon = 'https://chart.googleapis.com/chart?' +
+        'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+    var markersArray = [];
 
     var obj = {};
-    obj.getDistance = function(opts) {
-      var origin = opts.origin;
-      var destination = opts.destination;
-      var units = typeof opts.units !== 'undefined' ? opts.units : 'imperial'
-      var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?' +
-                'origins=' + origin + '&destinations=' + destination +
-                '&units=' + units + '&key=' + GMAPS_KEY;
-      console.log('GETting', url)
-      $http.get(url).then(function successCallback(res) {
-        data.origin = res.data.origin_addresses[0];
-        data.destination = res.data.destination_addresses[0];
-        data.distance = res.data.rows[0].elements[0].distance;
-        data.duration = res.data.rows[0].elements[0].duration;
-        data.status = res.data.rows[0].elements[0].status;
-        $rootScope.$broadcast('MAPS_DATA_CHANGED');
-      }, function errorCallback(res) {
-        console.log('response error:', res.status, res.statusText);
-      });
+    obj.getDistance = function(origin, destination) {
+      distMatService.getDistanceMatrix(
+        {
+          origins: [origin],
+          destinations: [destination],
+          travelMode: google.maps.TravelMode.DRIVING,
+          unitSystem: google.maps.UnitSystem.IMPERIAL,
+        }, getDistanceMatrixCallback
+      );
+
+      // set service data, show markers on map
+      function getDistanceMatrixCallback(res, status) {
+        if (status == google.maps.DistanceMatrixStatus.OK) {
+
+          data.origin.text = res.originAddresses[0];
+          data.destination.text = res.destinationAddresses[0];
+          data.distance = res.rows[0].elements[0].distance;
+          data.duration = res.rows[0].elements[0].duration;
+          data.status = res.rows[0].elements[0].status;
+
+          // clear markers from map
+          deleteMarkers(markersArray);
+
+          // returns origin or destination callback for geocode request
+          var showGeocodedAddressOnMap = function(asDestination) {
+            var icon = asDestination ? destinationIcon : originIcon;
+            return function(results, status) {
+              if (status === google.maps.GeocoderStatus.OK) {
+                // add lat, lng to data.origin or data.destination
+                if (asDestination) {
+                  data.destination.lat = results[0].geometry.location.lat();
+                  data.destination.lng = results[0].geometry.location.lng();
+                } else {
+                  data.origin.lat = results[0].geometry.location.lat();
+                  data.origin.lng = results[0].geometry.location.lng();
+                }
+                map.fitBounds(bounds.extend(results[0].geometry.location));
+                markersArray.push(new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location,
+                  icon: icon,
+                }));
+              } else {
+                console.log("Geocode error:", status);
+              }
+            };
+          };
+
+          // geocode origin and destination and mark on map
+          geocoder.geocode({address: data.origin.text},
+                           showGeocodedAddressOnMap(false));
+          geocoder.geocode({address: data.destination.text},
+                           showGeocodedAddressOnMap(true));
+
+          // broadcast the map data change
+          $timeout(function() {$rootScope.$broadcast('MAPS_DATA_CHANGED');}, 200);
+        } else {
+          console.log('getDistanceMatrix error:', res, status);
+        }
+      }
+
+      function deleteMarkers(markersArray) {
+        for (var i = 0; i < markersArray.length; i++) {
+          markersArray[i].setMap(null);
+        }
+        markersArray = [];
+      }
+    }
+
+    obj.getData = function() {
+      return data;
     };
-    obj.getData = function() {return data;};
 
     return obj;
   }
 ])
 
-.controller('TripCtrl', ['$scope', 'googleMapsService', function($scope, googleMapsService) {
-  $scope.something = 'a string';
-  $scope.callGetDistance = function () {
-    console.log('callGetDistance');
-    var opts = {
-      origin: $scope.origin,
-      destination: $scope.destination,
-    }
-    console.log(opts)
-    googleMapsService.getDistance(opts)
-  };
+.controller('TripCtrl', ['$scope', 'googleMapsService',
+  function($scope, googleMapsService) {
+    $scope.callGetDistance = function() {
+      googleMapsService.getDistance($scope.origin, $scope.destination)
+    };
 
-  $scope.$on('MAPS_DATA_CHANGED', function(response) {
-    $scope.mapsData = googleMapsService.getData();
-    console.log('loaded data into controller:', $scope.mapsData);
-  })
-}])
+    $scope.$on('MAPS_DATA_CHANGED', function() {
+      $scope.mapsData = googleMapsService.getData();
+    })
+  }
+])
 ;
